65dcb96544f41f802050637650aeb88d22fce395
Wingemo <philip.wingemo@outlook.com>
2021-09-26 13:30:11 +0200

    Add files via upload

---
+++ os-series-ep1/src/impl/x86_64/boot/kernel.c
@@ -0,0 +1,21 @@
+void kernel_entry() {
+    const char *string = "Welcome to my own os - Philip Wingemo";
+    volatile unsigned char *vid = (unsigned char*)0xb8000;
+    int index = 0;
+
+    while(*string != 0)
+    {
+        *(vid) = *string;
+        ++string;
+        vid+=2;
+    }
+
+      while (index < 80 * 25 * 2) {
+        *(vid) = ' ';
+        vid+=2;
+        index += 2;
+    }
+}
+    
+
+
