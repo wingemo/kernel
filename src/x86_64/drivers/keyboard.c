af8a9b3897c40f18a30cd0870021d7fce51f01af
Wingemo <philip.wingemo@outlook.com>
2021-09-28 01:40:57 +0200

    Update keyboard.c

--- src/x86_64/drivers/keyboard.c
+++ src/x86_64/drivers/keyboard.c
@@ -1,1 +1,18 @@
+unsigned char inb() {
+  unsigned char ret;
+  asm volatile("inb %1, %0": "=a"(ret): "dN"(0x60));
+  return ret;
+}
+
+void input_keyboard() {
+  unsigned char inputdata;
+  while ((inputdata = inb())) {
+    if (inputdata == 0x11) {
+      video_write("w");
+    } 
+    if (inputdata == 0x19) {
+      video_write("p");
+    } 
+  }
+}
 
