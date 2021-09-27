d2f80a73a5de107ecd60444319089a9e91761e8d
Wingemo <philip.wingemo@outlook.com>
2021-09-28 01:37:43 +0200

    Update screen.c

--- src/x86_64/drivers/screen.c
+++ src/x86_64/drivers/screen.c
@@ -1,1 +1,22 @@
+void clear_screen() {
+  char * p = (char * ) 0xb8000;
+  int i = 0;
+  for (i; i < 4000; i++) {
+    * p = '\0';
+    p++;
+  }
+}
+
+void output_screen(const unsigned char * string) {
+  unsigned char * p = (unsigned char * ) string;
+  p++;
+  char * c = (char * )(0xb8000);
+  while ( * p != '\0') {
+    * c = * p;
+    c++;
+    * c = 0x07;
+    c++;
+    p++;
+  }
+}
 
