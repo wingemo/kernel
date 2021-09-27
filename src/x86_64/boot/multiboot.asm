2c88e0c4c69e0dd864c0402b657e0f8181d69653
Wingemo <philip.wingemo@outlook.com>
2021-09-28 00:28:45 +0200

    Update multiboot.asm

--- src/x86_64/boot/multiboot.asm
+++ src/x86_64/boot/multiboot.asm
@@ -1,25 +1,10 @@
-global start
-extern kernel_entry 
-
-section .bss
-    resb 8192                        
-    stack:
-    
-section .boot
+section .multiboot
 _start:
-    dd 0xE85250D6     ; magic number
-    dd 0              ; protected mode code
-    dd _start - _end  ; length
-    dd 0x100000000 - (0xe85250d6 + 0 + (_start - _end))
-    dw 0  ; type
-    dw 0  ; flags
-    dd 8  ; size
+	dd 0xE85250D6     ; magic number
+	dd 0              ; protected mode code
+	dd _start - _end  ; length
+	dd 0x100000000 - (0xe85250d6 + 0 + (_start - _end))
+	dw 0  ; type
+	dw 0  ; flags
+	dd 8  ; size
 _end:
-
-section .text
-bits 32s
-start:
-   cli                     
-   mov esp, stack    
-   call kernel_entry
-   hlt       
