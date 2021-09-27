26e3da6b6e8be9c1479974051b945fdc3253190d
Wingemo <philip.wingemo@outlook.com>
2021-09-28 00:24:00 +0200

    Update kernel.c

--- src/x86_64/boot/kernel.c
+++ src/x86_64/boot/kernel.c
@@ -1,5 +1,20 @@
 #define MSG "Hello World!"
 
+void kernel_entry() {
+  clear_screen();
+  video_write(MSG);
+  keyboard();
+}
+
+void clear_screen() {
+  char * p = (char * ) 0xb8000;
+  int i = 0;
+  for (i; i < 4000; i++) {
+    * p = '\0';
+    p++;
+  }
+}
+
 void video_write(const unsigned char * string) {
   unsigned char * p = (unsigned char * ) string;
   p++;
@@ -13,16 +28,20 @@ void video_write(const unsigned char * string) {
   }
 }
 
-void clear_screen() {
-  char * p = (char * ) 0xb8000;
-  int i = 0;
-  for (i; i < 4000; i++) {
-    * p = '\0';
-    p++;
-  }
+
+unsigned char inb()
+{
+  unsigned char ret;
+  asm volatile("inb %1, %0" : "=a"(ret) : "dN"(0x60));
+  return ret;
 }
 
-void kernel_entry() {
-  clear_screen();
-  video_write(MSG);
+void keyboard()
+{
+    unsigned char inputdata;
+    while((inputdata = inb())){
+        if (inputdata == 0x11) {
+            video_write("w");
+        }
+    }
 }
