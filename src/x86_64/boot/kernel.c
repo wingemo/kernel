eda33953cab773f52c8282b677d97fde1a4fd076
Wingemo <philip.wingemo@outlook.com>
2021-09-27 23:48:54 +0200

    Update kernel.c

--- src/x86_64/boot/kernel.c
+++ src/x86_64/boot/kernel.c
@@ -1,30 +1,28 @@
 #define MSG "Hello World!"
 
 void kernel_entry() {
-    clear_screen();
-    video_write(MSG);
+  clear_screen();
+  video_write(MSG);
 }
 
-void clear_screen(){
-    char *p = (char *)0xb8000;
-    int i = 0 ;                                                                                                                                  
-    for(i ; i < 4000 ; i++){                                                                                                                                                       
-            *p = '\0';                                                                                                    
-            p++ ;                                                                                                         
-    }        
+void clear_screen() {
+  char * p = (char * ) 0xb8000;
+  int i = 0;
+  for (i; i < 4000; i++) {
+    * p = '\0';
+    p++;
+  }
 }
 
-void video_write(const unsigned char *string){                                                                 
-    unsigned char *p = (unsigned char*) string;       
-    p++;                                            
-    char *c = (char*) (0xb8000);                                                                                                                                                                                         
-    while(*p != '\0')                                                                                                    
-    {                                                                                                                
-            *c = *p;                                                                                                 
-            c++ ;                                                                                                       
-            *c = 0x07;                                                                                                
-            c++ ;     
-            p++ ;                                                                                                                                                                                                             
-    }    
+void video_write(const unsigned char * string) {
+  unsigned char * p = (unsigned char * ) string;
+  p++;
+  char * c = (char * )(0xb8000);
+  while ( * p != '\0') {
+    * c = * p;
+    c++;
+    * c = 0x07;
+    c++;
+    p++;
+  }
 }
-
