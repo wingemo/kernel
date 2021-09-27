0796f050a272e4f7c3376cd59b2e3827169ff107
Wingemo <philip.wingemo@outlook.com>
2021-09-28 00:29:10 +0200

    Update kernel.asm

--- src/x86_64/boot/kernel.asm
+++ src/x86_64/boot/kernel.asm
@@ -1,1 +1,16 @@
+global start
+extern kernel_entry 
+
+section .bss
+    resb 8192                        
+    stack:
+
+section .text
+bits 32s
+start:
+   cli                     
+   mov esp, stack    
+   call kernel_entry
+   hlt                     
+
 
