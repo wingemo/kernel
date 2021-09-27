4d8062e632c5f34ccbf719da81a587f44d38acb8
Wingemo <philip.wingemo@outlook.com>
2021-09-27 23:47:56 +0200

    Update kernel.c

--- src/x86_64/boot/kernel.c
+++ src/x86_64/boot/kernel.c
@@ -1,17 +1,31 @@
+#define MSG "Hello World!"
+
 void kernel_entry() {
-    const char *string = "Welcome to my own os - Philip Wingemo";
-    volatile unsigned char *vid = (unsigned char*)0xb8000;
-    int index = 0;
-
-    while(*string != 0) {
-        *(vid) = *string;
-        ++string;
-        vid+=2;
-    }
-
-    while (index < 80 * 25 * 2) {
-        *(vid) = ' ';
-        vid+=2;
-        index += 2;
-    }
+    clear_screen();
+    video_write(MSG);
 }
+
+void clear_screen(){
+    char *p = (char *)0xb8000;
+    int i = 0 ;                                                                                                                                  
+    for(i ; i < 4000 ; i++){                                                                                                                                                       
+            *p = '\0';                                                                                                    
+            p++ ;                                                                                                         
+    }        
+}
+
+
+void video_write(const unsigned char *string){                                                                 
+    unsigned char *p = (unsigned char*) string;       
+    p++;                                            
+    char *c = (char*) (0xb8000);                                                                                                                                                                                         
+    while(*p != '\0')                                                                                                    
+    {                                                                                                                
+            *c = *p;                                                                                                 
+            c++ ;                                                                                                       
+            *c = 0x07;                                                                                                
+            c++ ;     
+            p++ ;                                                                                                                                                                                                             
+    }    
+}
+
