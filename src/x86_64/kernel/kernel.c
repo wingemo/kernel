0877166dcac8f134db26edc88365aec19e066299
Wingemo <philip.wingemo@outlook.com>
2021-09-28 08:49:26 +0200

    Update kernel.c

--- src/x86_64/kernel/kernel.c
+++ src/x86_64/kernel/kernel.c
@@ -1,5 +1,9 @@
 #define MSG "Hello World!"
 
+/*
+ * The following two APIs (cpu_maps_update_begin/done) must be used when
+ * attempting to serialize the updates to cpu_online_mask & cpu_present_mask.
+ */
 void start_kernel() {
   clear_screen();
   output_screen();
