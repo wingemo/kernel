5f32882d65694c95c5ede2c06197a9ac516d2a89
Wingemo <philip.wingemo@outlook.com>
2021-09-28 08:50:04 +0200

    Update screen.c

--- src/x86_64/drivers/screen.c
+++ src/x86_64/drivers/screen.c
@@ -1,3 +1,9 @@
+/* CPU control.
+ * (C) 2001, 2002, 2003, 2004 Rusty Russell
+ *
+ * This code is licenced under the GPL.
+ */
+
 /*
  * The following two APIs (cpu_maps_update_begin/done) must be used when
  * attempting to serialize the updates to cpu_online_mask & cpu_present_mask.
