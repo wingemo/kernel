d4a5473c6333c6a64f9a6f098f3c2a274af67ce7
Wingemo <philip.wingemo@outlook.com>
2021-09-28 15:44:46 +0200

    Update boot.asm

--- src/x86_64/boot/boot.asm
+++ src/x86_64/boot/boot.asm
@@ -5,17 +5,6 @@ section .bss
      resb 8192                        
      stack:
 
-section .multiboot
-_start:
-	dd 0xE85250D6     ; magic number
-	dd 0              ; protected mode code
-	dd _start - _end  ; length
-	dd 0x100000000 - (0xe85250d6 + 0 + (_start - _end))
-	dw 0  ; type
-	dw 0  ; flags
-	dd 8  ; size
-_end:
-
  section .text
  bits 32s
  start:
