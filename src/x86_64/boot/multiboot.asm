ebedacf1101a996a8b26c891096c399310ee4671
Wingemo <philip.wingemo@outlook.com>
2021-09-28 16:34:10 +0200

    Update multiboot.asm

--- src/x86_64/boot/multiboot.asm
+++ src/x86_64/boot/multiboot.asm
@@ -3,7 +3,7 @@ _start:
 	dd 0xE85250D6     ; magic number
 	dd 0              ; protected mode code
 	dd _start - _end  ; length
-	dd 0x100000000 - (0xe85250d6 + 0 + (_start - _end))
+	dd 0x100000000 - (0xe85250d6 + 0 + (_start - _end)) ; 
 	dw 0  ; type
 	dw 0  ; flags
 	dd 8  ; size
