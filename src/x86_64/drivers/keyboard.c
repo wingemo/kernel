9eaaec160f8c9e6f067e15cce4ce5095fc8594ee
Wingemo <philip.wingemo@outlook.com>
2021-09-28 08:48:20 +0200

    Update keyboard.c

--- src/x86_64/drivers/keyboard.c
+++ src/x86_64/drivers/keyboard.c
@@ -1,9 +1,23 @@
+/* CPU control.
+ * (C) 2001, 2002, 2003, 2004 Rusty Russell
+ *
+ * This code is licenced under the GPL.
+ */
+
+/*
+ * The following two APIs (cpu_maps_update_begin/done) must be used when
+ * attempting to serialize the updates to cpu_online_mask & cpu_present_mask.
+ */
 unsigned char inb() {
   unsigned char ret;
   asm volatile("inb %1, %0": "=a"(ret): "dN"(0x60));
   return ret;
 }
 
+/*
+ * The following two APIs (cpu_maps_update_begin/done) must be used when
+ * attempting to serialize the updates to cpu_online_mask & cpu_present_mask.
+ */
 void listen_keyboard() {
   unsigned char inputdata;
   while ((inputdata = inb())) {
